efa4a310aca813755e7649655f7e9668f52ecdc8
Princy Sultania <princyjune66@gmail.com>
2020-11-26 13:49:58 +0530

    1st coment

--- mnistcnn.py
+++ mnistcnn.py
@@ -1,4 +1,1 @@
-[D[A[C[C[C[C[C[C[C[C [C[C[C[C[C[C[C[C[C[C
-
-[A[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cmnistcnn.py[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[D[C[C[C[C[C[C[C[Ccat > mnistcnn.py
-print()[D"CNN code")
+print("CNN code")
