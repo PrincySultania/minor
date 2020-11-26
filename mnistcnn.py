5c0beef351c82c036638573b183835da40f98498
Princy Sultania <princyjune66@gmail.com>
2020-11-26 13:34:40 +0530

    1st commit

---
+++ mnistcnn.py
@@ -0,0 +1,4 @@
+[D[A[C[C[C[C[C[C[C[C [C[C[C[C[C[C[C[C[C[C
+
+[A[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cmnistcnn.py[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[D[C[C[C[C[C[C[C[Ccat > mnistcnn.py
+print()[D"CNN code")
